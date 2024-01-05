d3bb1a299f872531c45f2d2bfa745e548e69a7b3
SaintHazzard <chamitos1998@hotmail.com>
2024-01-05 06:38:04 -0500

    llaves de sujetos test arregladas

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ registroAspirantes = {}
 rutas={
   
 }
-registroAspirantes['1']= Camper('1097910340', 'Oviel Felipe', 'Mendoza Pineda', 3165880900, 'Cir 36a 104-128 Altos de la Pradera T3-1204', 'Martiza Pineda Celis', 'Aspirante','')
-registroAspirantes['0'] = Camper('1098808399','Alejandro','Martinez Santos',3214321455,'Monviso','Mi madresita','Aspirante','')
+registroAspirantes['1097910340']= Camper('1097910340', 'Oviel Felipe', 'Mendoza Pineda', 3165880900, 'Cir 36a 104-128 Altos de la Pradera T3-1204', 'Martiza Pineda Celis', 'Aspirante','')
+registroAspirantes['1098808399'] = Camper('1098808399','Alejandro','Martinez Santos',3214321455,'Monviso','Mi madresita','Aspirante','')
 
 for i in registroAspirantes:
   print('-'*50)
