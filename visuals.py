521aa0f52c5175fd84180a063758a3d61bcdfe67
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-01-05 09:18:30 -0500

    Track

---
+++ visuals.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+def sleep():
+    time.sleep(1)
+
+def clear():
+    # os.system('cls')
+    os.system('clear')
+def wait():
+     input("Presiona Enter para continuar...")
+   
+
