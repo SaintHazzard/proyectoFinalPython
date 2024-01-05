932a5e909b29642561f7b1be977df4d7e966ec4d
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-01-05 06:20:21 -0500

    Corregido estado camper.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from persona import *
 
 registroAspirantes = {}
-registroAspirantes['1']= Camper('1097910340', 'Oviel Felipe', 'Mendoza Pineda', 3165880900, 'Cir 36a 104-128 Altos de la Pradera T3-1204', 'Martiza Pineda Celis', 'Aspitante','NodeJs')
+registroAspirantes['1']= Camper('1097910340', 'Oviel Felipe', 'Mendoza Pineda', 3165880900, 'Cir 36a 104-128 Altos de la Pradera T3-1204', 'Martiza Pineda Celis', 'Aspirante','NodeJs')
 registroAspirantes['0'] = Camper('1098808399','Alejandro','Martinez Santos',3214321455,'Monviso','Mi madresita','Aspirante','Java')
 
 for i in registroAspirantes:
