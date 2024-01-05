19c4bd948b85c8aa4d4057438bde5c0fee0c7bf3
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-01-04 21:35:12 -0500

    Creado clase entrenador y modificado clase camper.

--- persona.py
+++ persona.py
@@ -7,7 +7,14 @@ class Persona:
         self. direccion = direccion
 
 class Camper(Persona):
-    def __init__(self, documento, nombres, apellidos, telefono, direccion, acudiente, estado) -> Persona:
+    def __init__(self, documento, nombres, apellidos, telefono, direccion, acudiente, estado, ruta) -> Persona: #Investigar si es necesario esto
         super().__init__(documento, nombres, apellidos, telefono, direccion)
         self.acudiente = acudiente
         self.estado = estado
+        self.ruta = ruta
+        
+class Entrenador(Persona):
+    def __init__(self, documento, nombres, apellidos, telefono, direccion, ruta, horario) -> Persona: #Investigar si es necesario esto
+        super().__init__(documento, nombres, apellidos, telefono, direccion)
+        self.ruta = ruta
+        self.horario = horario
