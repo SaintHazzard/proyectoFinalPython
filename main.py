fac9baafec2db85366d1c95c3df8e0a8be6eef6e
SaintHazzard <chamitos1998@hotmail.com>
2024-01-05 06:43:47 -0500

    movil y fijo, telefonos solucionado

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ registroAspirantes = {}
 rutas={
   
 }
-registroAspirantes['0']= Camper('1097910340', 'Oviel Felipe', 'Mendoza Pineda', 3165880900, 'Cir 36a 104-128 Altos de la Pradera T3-1204', 'Martiza Pineda Celis', 'Aspirante','')
-registroAspirantes['1'] = Camper('1098808399','Alejandro','Martinez Santos',3214321455,'Monviso','Mi madresita','Aspirante','')
+registroAspirantes['0']= Camper('1097910340', 'Oviel Felipe', 'Mendoza Pineda',3165880900,123123456, 'Cir 36a 104-128 Altos de la Pradera T3-1204', 'Martiza Pineda Celis', 'Aspirante','')
+registroAspirantes['1'] = Camper('1098808399','Alejandro','Martinez Santos',3214321455,8883599,'Monviso','Mi madresita','Aspirante','')
 
 for i in registroAspirantes:
   print('-'*50)
--- persona.py
+++ persona.py
@@ -12,9 +12,8 @@ class Persona:
          print(f'Documento: {self.documento}\nNombre: {self.nombres}\nApellidos: {self.apellidos}\nRuta: {self.telefonos}')
 
 class Camper(Persona):
-    def __init__(self, documento, nombres, apellidos, 
-                 telefono, direccion, acudiente, estado, ruta) -> Persona: #Investigar si es necesario esto
-        super().__init__(documento, nombres, apellidos, telefono, direccion)
+    def __init__(self, documento, nombres, apellidos, movil,fijo, direccion, acudiente, estado, ruta) -> Persona: #Investigar si es necesario esto
+        super().__init__(documento, nombres, apellidos, movil,fijo, direccion)
         self.acudiente = acudiente
         self.estado = estado
         self.ruta = ruta
@@ -22,8 +21,8 @@ class Camper(Persona):
       print(f'Documento: {self.documento}\nNombre: {self.nombres}\nApellidos: {self.apellidos}\n Estado: {self.estado}\nRuta: {self.ruta}')
         
 class Entrenador(Persona):
-    def __init__(self, documento, nombres, apellidos, telefono, direccion, ruta, horario) -> Persona: #Investigar si es necesario esto
-        super().__init__(documento, nombres, apellidos, telefono, direccion)
+    def __init__(self, documento, nombres, apellidos, movil,fijo, direccion, ruta, horario) -> Persona: #Investigar si es necesario esto
+        super().__init__(documento, nombres, apellidos, movil,fijo, direccion)
         self.ruta = ruta
         self.horario = horario
     
