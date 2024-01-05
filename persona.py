db72e8a60948255c2dc39131f1965a7d9c8bf9ff
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-01-04 21:24:30 -0500

    Creado clase persona.

---
+++ persona.py
@@ -0,0 +1,8 @@
+class Persona:
+    def __init__(self, documento, nombres, apellidos, telefono, direccion) -> object:
+        self.documento = documento
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.telefono = telefono
+        self. direccion = direccion
+        
