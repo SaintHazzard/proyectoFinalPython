d2271959e30adb162f442f4f2cae7e3d3e0da436
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-01-05 14:32:34 -0500

    commit

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import os
 import json
 import pathlib
 from visuals import *
-clear()
+
 
 registroAspirantes = {}
 rutas={
   
 }
-registroAspirantes['0']= Camper('1097910340', 'Oviel 2', 'Mendoza Pineda',3165880900,123123456, 'Cir 36a 104-128 Altos de la Pradera T3-1204', 'Martiza Pineda Celis', 'Aspirante','')
+# registroAspirantes['0']= Camper('1097910340', 'Oviel 2', 'Mendoza Pineda',3165880900,123123456, 'Cir 36a 104-128 Altos de la Pradera T3-1204', 'Martiza Pineda Celis', 'Aspirante','')
 
 
   
@@ -27,10 +27,23 @@ def from_JSOn():
   return acum
  
 temporalDatosJson = from_JSOn()
+print(temporalDatosJson)
+def procesar_temporal_datos(temporalDatosJson):
+    for id in temporalDatosJson:
+        print(temporalDatosJson[id])
 
-# print(temporalDatosJson)
-for ind in temporalDatosJson:
-  print(temporalDatosJson[ind])
-  registroAspirantes[ind] = Camper(temporalDatosJson[ind]['documento'])
-    
+        documento = temporalDatosJson[id]['documento']
+        nombres = temporalDatosJson[id]['nombres']
+        apellidos = temporalDatosJson[id]['apellidos']
+        movil = temporalDatosJson[id]['movil']
+        fijo = temporalDatosJson[id]['fijo'] 
+        direccion = temporalDatosJson[id]['direccion']
+        acudiente = temporalDatosJson[id]['acuddiente']
+        estado = temporalDatosJson[id]['estadon']
+        ruta = temporalDatosJson[id]['ruta']
+        registroAspirantes[documento] =  Camper(documento, nombres, apellidos, movil, fijo, direccion, acudiente, estado, ruta)
 
+
+procesar_temporal_datos(temporalDatosJson)
+for documento in registroAspirantes:
+ registroAspirantes[documento].showData()
