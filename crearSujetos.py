fa7155124460d916004ae234e87a2fb19802ab5d
SaintHazzard <chamitos1998@hotmail.com>
2024-01-06 05:55:57 -0500

    Sujetos de prueba enumerados

--- crearSujetos.py
+++ crearSujetos.py
@@ -1,9 +1,9 @@
 from classes.classCamper import *
 registroAspirantes={}
-registroAspirantes['0']= Camper('1097910340', 'Oviel Felipe', 'Mendoza Pineda',3165880900,123123456, 'Cir 36a 104-128 Altos de la Pradera T3-1204', 'Martiza Pineda Celis')
+registroAspirantes['1']= Camper('1', 'Oviel Felipe', 'Mendoza Pineda',3165880900,123123456, 'Cir 36a 104-128 Altos de la Pradera T3-1204', 'Martiza Pineda Celis')
 
 
-registroAspirantes['1'] = Camper('1',
+registroAspirantes['0'] = Camper('0',
                                   'Alejandro',
                                   'Martinez Santos',
                                   3214321455,
@@ -13,9 +13,9 @@ registroAspirantes['1'] = Camper('1',
 
 
 
-registroAspirantes['2']= Camper('1097910341', 'Oviel 2', 'Mendoza Pineda',3165880900,123123456, 'Cir 36a 104-128 Altos de la Pradera T3-1204', 'Martiza Pineda Celis')
+registroAspirantes['2']= Camper('2', 'Oviel 2', 'Mendoza Pineda',3165880900,123123456, 'Cir 36a 104-128 Altos de la Pradera T3-1204', 'Martiza Pineda Celis')
 
-registroAspirantes['3']= Camper('1091356862', 'Sofia Marcela', 'Medina Diaz',3165880900,123123456 , 'Cir 36a 104-128 Altos de la Pradera T3-1204', 'Martiza Pineda Celis')
+registroAspirantes['3']= Camper('3', 'Sofia Marcela', 'Medina Diaz',3165880900,123123456 , 'Cir 36a 104-128 Altos de la Pradera T3-1204', 'Martiza Pineda Celis')
 
 
 
