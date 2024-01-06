889873cdde8e7cea3610dfb5166fc7f3e2694ea1
SaintHazzard <chamitos1998@hotmail.com>
2024-01-05 19:44:32 -0500

    error aun

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from classes.persona import * 
 from toolsTest.visuals import *
 from processJsonToMain import *
-from classes.classCamper import *
+
 
 registroAspirantes = {
   
@@ -10,29 +10,6 @@ rutas={
   
 }
 
-class Persona:
-    def __init__(self, documento, nombres, apellidos, movil,fijo, direccion) -> object:
-        self.documento = documento
-        self.nombres = nombres
-        self.apellidos = apellidos
-        self.telefonos = {'movil': movil,
-                          'fijo': fijo}
-        self. direccion = direccion
-    def getNombreCompleto(self) -> str:
-      return f'{self.nombres} {self.apellidos}'
-    
-    def showData(self):
-         print(f'Documento: {self.documento}\nNombre: {self.nombres}\nApellidos: {self.apellidos}\nTelefonos: \n\tMovil: {self.telefonos["movil"]}\n\tFijo: {self.telefonos["fijo"]}')
-         
-    
-    def crearJson(self):
-        # Ruta del archivo donde guardarás el JSON
-      ruta_archivo = f"jsonData/{self.nombres}.json"
-      # Guardar los datos como JSON en un archivo
-      with open(ruta_archivo, 'w+') as archivo_json:
-          json.dump(self.to_JSON(), archivo_json,indent=4)
-
-      print(f"Datos guardados en {ruta_archivo}")
 
 while True:
   temporalDatosJson = from_JSOn()
